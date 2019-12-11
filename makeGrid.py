ca5e524214f8432064b0c8cf66ea2351196c3a5f
Alex Chapple <acha809@UoA.auckland.ac.nz>
2019-12-12 11:35:31 +1300

    test7

--- makeGrid.py
+++ makeGrid.py
@@ -7,10 +7,10 @@ owd = os.getcwd
 
 ### Set values ###
 
-Energy_guess = 10**52
-Nickel_mass = 10**-0.75
-alpha = 0.95
-Nickel_boundary_mass = 11.83 + (15.541415935387822955 - 11.83)*alpha
+Energy = 52
+Nickel_mass = -0.75
+total_mass = 15.541415935387822955
+excised = 11.83
 
 # range = [-0.3, -0.2, -0.1, 0, 0.1, 0.2, 0.3]
 # alpha_range = [0.1, 0.3, 0.5, 0.7, 0.9]
@@ -29,29 +29,37 @@ for i in range:
 for i in permutations:
 
     ### Makes directory ~/output/index of number, and then copies in a parameter file into there
+    original = open('original_parameters','r')
 
     os.mkdir('output/' + str(permutations.index(i)))
-    shutil.copy('original_parameters', 'output/' + str(permutations.index(i)))
+    #shutil.copy('original_parameters', 'output/' + str(permutations.index(i)))
 
     ### changes directorty into the index number directory and opens the parameter file to edit
     os.chdir('output/' + str(permutations.index(i)))
-    parameters_file = open('original_parameters', 'r+')
+    parameters_file = open('parameters', 'w')
 
     ### Edits line for that grid parameter
+    line_num = 0
     for line in parameters_file:
-        if line.split(None, 1)[0] == 'final_energy':
-            line.replace(line, 'final_energy = ' + str(Energy_guess + i[0]))
-            parameters_file.write(line)
-        
-        elif line.split(None, 1)[0] == 'Ni_mass':
-            line.replace(line, 'Ni_mass = ' + str( Nickel_mass + i[1]))
-            parameters_file.write(line)            
+        line_num += 1
 
-        elif line.split(None, 1)[0] == 'Ni_boundary_mass':
-            line.replace(line, 'Ni_boundary_mass = ' + str(11.83 + (15.541415935387822955 - 11.83)*i[2]))
+        if line_num == 19:
+            newline = 'final_energy = ' + str(format(10**(Energy + i[0]), '.6e'))
+            parameters_file.write(newline)
+            parameters_file.write('\n')
+        elif line_num == 49:
+            newline = 'Ni_mass = ' + str(format(10**(Nickel_mass + i[1]), '.6e'))
+            parameters_file.write(newline)
+            parameters_file.write('\n')
+        elif line_num == 50:
+            newline = 'Ni_boundary_mass = ' + str( format( excised + (total_mass - excised)*i[2] ) )
+            parameters_file.write(newline)
+            parameters_file.write('\n')
+        else:
             parameters_file.write(line)
     
     parameters_file.close()
+    original.close()
     os.chdir(owd)
 
     
