4f6efd02368794b8d3918ff896d2656595c68048
Alex Chapple <acha809@UoA.auckland.ac.nz>
2019-12-13 08:09:07 +1300

    plotter updated

--- plot_data.py
+++ plot_data.py
@@ -2,4 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 for i in range(0,244):
-    
+
+    data = open('/output/' + str(i) + '/data/lum_observed.dat')
+
+    seconds = data[:,0]
+    luminosity = data[:,1]
+
+    days = [i / (24 * 60 * 60) for i in seconds]
+    log_luminosity = [np.log10(i) for i in luminosity]
+
+    plt.plot(days, log_luminosity)
+
+plt.show()
